9c1232031af0490f94d6721fe8d29e9e97aa07d2
smg8n <60581192+smg8n@users.noreply.github.com>
2020-02-17 21:11:38 +0530

    update main
    added help header file

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-
+#include "help.h"
 int main(int argc, char *argv[])
 {
 	int n = 0;
@@ -22,14 +22,7 @@ int main(int argc, char *argv[])
 		switch(c)
     		{
 	case 'h':
-               	printf("\nInvocation: oss [-h] [-n x | -s x | -b B | -i I ] [-o (filename)]\n");
-               	printf("-------------------------------------------------Program Options--------------------------------------------------\n");
-               	printf("       -h             Describe how the project should be run and then, terminate\n");
-               	printf("       -n x           Indicate the maximum total of child processes oss will ever create. (Default 4)\n");
-               	printf("       -s x           Indicate the number of children allowed to exist in the system at the same time. (Default 2)\n");
-               	printf("       -b B           Start of the sequence of numbers to be tested for primality\n");
-               	printf("       -i I           Increment between numbers that we test\n");
-               	printf("       -o filename    Output file\n");
+               	help();
                	return EXIT_SUCCESS;
       	case 'n':
       		n = atoi(optarg);
