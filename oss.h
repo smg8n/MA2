45b8cba4974b4bddfd0f989b3b4de33ccac3ef1b
smg8n <60581192+smg8n@users.noreply.github.com>
2020-02-24 14:40:08 -0600

    Now shared memory is used to send prime/nonprime info back to parent

---
+++ oss.h
@@ -0,0 +1,13 @@
+
+#define SHMKEY_CLOCK 123456
+#define BUFF_SIZE 2*sizeof(int)
+
+#define MAX_CHILD_PROCESSES 10240
+#define SHMKEY_ARRAY 1234567
+#define ARRAY_BUFF_SIZE (MAX_CHILD_PROCESSES*sizeof(int))
+
+struct Clock{
+    int seconds;
+    int nanoseconds;
+    long totaltimeconsumed;
+};
